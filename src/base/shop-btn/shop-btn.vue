820aa8da7da6b31a28cfafe75d8250fe4fb12f1c
Grace1025 <Grace1025@Grace1025deMacBook-Air.local>
2017-11-01 10:54:58 +0800

    购物车图标
    购物车图标增加

--- src/base/shop-btn/shop-btn.vue
+++ src/base/shop-btn/shop-btn.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="shop_btn">
-    <div class="shopcar_img"></div>
+    <div class="shopcar_img">
+      <span class="icon-shopping_cart"></span>
+      <span class="num" v-show="true">12</span>
+    </div>
     <div class="now_buy">立即购买</div>
     <div class="add_shopcar">加入购物车</div>
   </div>
@@ -17,10 +20,30 @@
     width: 100%
     height: 42px
     line-height: 42px
-    border-top: 1px solid #b1b1b1
+    border-top: 1px solid #f1f1f1
     background: #fff
-    .shop_img
+    .shopcar_img
+      position: relative
       float: left
+      padding-top: 6px
+      margin-left: 36px
+      .icon-shopping_cart
+        font-size: 32px
+      .num
+        position:absolute
+        top: 0px
+        left: 25px
+        text-align: center
+        -webkit-border-radius: 50%
+        -moz-border-radius: 50%
+        border-radius: 50%;
+        background: rgb(240,20,20);
+        box-shadow: 0 4px 8px 0 rgba(0,0,0,0.4);
+        width: 18px
+        height: 18px
+        line-height: 18px
+        color:#fff
+        font-size: 13px
     .add_shopcar
       float: right
       padding-right: 10px
